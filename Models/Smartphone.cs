4f4cb223b721de208f85724cfffef4d5968a6c22
venturiniandre <venturiniandreee@gmail.com>
2024-01-09 22:57:27 -0300

    Yes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,24 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        protected string Modelo;
+
+        protected string Imei;
+
+        protected int Memoria;
 
         public Smartphone(string numero)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +31,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void DetalhesSmartphone()
+        {
+            Console.WriteLine($"Smartphone modelo: {Modelo}, imei: {Imei}, memoria: {Memoria} e numero: {Numero}.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("New Smartphone Nokia");
+Smartphone nokia = new Nokia (numero: "827425493", modelo: "Modelo Nokia 5", imei: "846198689", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.DetalhesSmartphone();
+
+Console.WriteLine("\n");
+Console.WriteLine("---------------------");
+Console.WriteLine("---------------------");
+Console.WriteLine("\n");
+
+Console.WriteLine("New Smartphone Iphone");
+Smartphone iphone = new Iphone (numero: "639451856", modelo: "Modelo Iphone 12", imei: "256584443", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("XP Investimentos");
+iphone.DetalhesSmartphone();
